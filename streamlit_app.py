abb53bca7b3c261bd8a56c306230ff9988c71a4f
Olivier Terrier <olivier.terrier@kairntech.com>
2021-11-24 10:10:33 +0100

    try spacy

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ Example using the components provided by spacy-streamlit in an existing app.
 Prerequisites:
 python -m spacy download en_core_web_sm
 """
-from spacy_streamlit import visualize
+from visualizer import visualize
 
 models = ["en_core_web_sm", "en_core_web_md"]
 default_text = "Sundar Pichai is the CEO of Google."
