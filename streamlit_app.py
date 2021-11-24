87401e28aaaa281815241acd1bc8e7a02d90a1e7
Olivier Terrier <olivier.terrier@kairntech.com>
2021-11-24 17:01:58 +0100

    try spacy

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,18 @@ python -m spacy download en_core_web_sm
 import streamlit as st
 from visualizer import visualize
 
-default_text = "Sundar Pichai is the CEO of Google."
+default_text = """Un retour suite à votre accueil me semble évident.
+
+Je suis satisafaite de la MAAF.
+
+Ce que je veux c' est avoir la meilleure couverture à tous points.
+Peut-être vais-je ramener tous mes contrats à la MAAF ou bien à la MAIF?
+Pour tout grouper.
+
+Très cordialement,
+
+Mme Valérie LAVRIL
+"""
 # Reads
 projects = None
 plans = None
