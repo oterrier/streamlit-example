3fed5d39be12b4e5fb76219929e4c8a511d8435c
Olivier Terrier <olivier.terrier@kairntech.com>
2021-11-26 18:21:39 +0100

    First beta version

--- util.py
+++ util.py
@@ -15,7 +15,7 @@ from streamlit.uploaded_file_manager import UploadedFile, UploadedFileRec
 def get_token(server: str, user: str, password: str):
     url = f"{server}/api/auth/login"
     auth = {"email": user, "password": password}
-    r = requests.post(url, json=auth,
+    r = requests.post(url, json=auth, params={'loginOnly':True},
                              headers={'Content-Type': "application/json", 'Accept': "application/json"},
                              verify=False)
     if r.ok:
