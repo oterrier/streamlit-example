b6b9376732020c9a51851ffd32a04b4341807dcf
Olivier Terrier <olivier.terrier@kairntech.com>
2021-12-01 15:46:36 +0100

    Better vizu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,11 @@
 import streamlit as st
 from sherpa_streamlit import visualize
 
-default_text = """Un retour suite à votre accueil me semble évident.
-Je suis un fidèle depuis des années.
-Ce que je veux c' est avoir la meilleure couverture à tous points.
-Peut-être vais-je ramener tous mes contrats à la MAAF ou bien à la MAIF ou alors AXA?
-Très cordialement,
+default_text = """This is a sample text.
+With two lines.
 """
 # Reads
-projects = None
-annotators = None
 st.set_page_config(
-    page_title="Blabla",
     layout="wide",
     initial_sidebar_state="expanded"
 )
